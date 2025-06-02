a4b1884033832e18290e76b57994e5d51d3c8441
Naiem-Devs <dnsnaiem@gmail.com>
2025-06-03 00:37:28 +0600

    done

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,4 +2,5 @@ document.querySelector(".siteBar-btn").addEventListener("click", function (event
 	event.preventDefault();
 	this.classList.toggle("active");
 	document.querySelector(".mobile-menu").classList.toggle("siteBar");
+	document.querySelector("body").classList.toggle("overflow-hidden");
 });
